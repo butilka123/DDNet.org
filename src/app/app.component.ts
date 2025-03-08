f8d4cd9334e679618d8d03c293241d0ecb45ee37
butilka123 <66038983+butilka123@users.noreply.github.com>
2025-03-09 00:57:21 +0100

    add breadcrumb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
 import { CoreModule } from './core/core.module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, CoreModule],
+  imports: [RouterModule, CoreModule, BreadcrumbModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  router = inject(Router);
+
+  items = this.getBreadcrumbItems();
+  home = { icon: 'pi pi-home', routerLink: '/' };
+
+  constructor() {
+    this.router.events.subscribe(() => {
+      this.items = this.getBreadcrumbItems();
+    });
+  }
+
+  getBreadcrumbItems() {
+    const segments = this.router.url.split('/').filter((segment) => segment);
+    return segments.map((segment, index) => ({
+      label: segment,
+      routerLink: '/' + segments.slice(0, index + 1).join('/'),
+    }));
+  }
+}

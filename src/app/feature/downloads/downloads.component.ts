04964c38b7666d5be6aa4a8c6c916e0fe9131a4b
butilka123 <66038983+butilka123@users.noreply.github.com>
2025-03-09 03:15:23 +0100

    add dummy download page

--- src/app/feature/downloads/downloads.component.ts
+++ src/app/feature/downloads/downloads.component.ts
@@ -1,11 +1,133 @@
 import { Component } from '@angular/core';
+import { ChipModule } from 'primeng/chip';
+import { PanelModule } from 'primeng/panel';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-downloads',
-  imports: [],
+  imports: [TagModule, ChipModule, PanelModule, ScrollPanelModule, TableModule],
   templateUrl: './downloads.component.html',
-  styleUrl: './downloads.component.scss'
+  styleUrl: './downloads.component.scss',
 })
 export class DownloadsComponent {
+  downloads = [
+    {
+      label: 'Windows 64bit',
+      url: 'https://ddnet.org/downloads/DDNet-19.0-win64.zip',
+    },
+    {
+      label: 'Windows ARM',
+      url: 'https://ddnet.org/downloads/DDNet-19.0-win-arm64.zip',
+    },
+    {
+      label: 'Windows 32bit',
+      url: 'https://ddnet.org/downloads/DDNet-19.0-win32.zip',
+    },
+    {
+      label: 'Linux x86',
+      url: 'https://ddnet.org/downloads/DDNet-19.0-linux_x86.tar.xz',
+    },
+    {
+      label: 'Linux x86-64',
+      url: 'https://ddnet.org/downloads/DDNet-19.0-linux_x86_64.tar.xz',
+    },
+    {
+      label: 'macOS (arm64 & x86-64)',
+      url: 'https://ddnet.org/downloads/DDNet-19.0-macos.dmg',
+    },
+    {
+      label: 'Android',
+      url: 'https://ddnet.org/downloads/DDNet-19.0.apk',
+    },
+    {
+      label: 'Source',
+      url: 'https://ddnet.org/downloads/DDNet-19.0.tar.xz',
+    },
+  ];
 
+  changes = [
+    {
+      tag: 'Client',
+      changes: ' Add spectator count to HUD',
+      authors: ['KebsCS'],
+    },
+    {
+      tag: 'Client',
+      changes: 'Auto-sync player camera option',
+      authors: ['TsFreddie'],
+    },
+    {
+      tag: 'Client',
+      changes: 'Teleport hookline (when 1 exit)',
+      authors: ['art0007i'],
+    },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+    { tag: '', changes: '', authors: [''] },
+  ];
+
+  secondaryDownloads = [
+    {
+      label: 'Windows 64bit',
+      url: 'https://ddnet.org/downloads/DDNet-19.0-win64.zip',
+    },
+    {
+      label: 'Windows ARM',
+      url: 'https://ddnet.org/downloads/DDNet-19.0-win-arm64.zip',
+    },
+    {
+      label: 'Windows 32bit',
+      url: 'https://ddnet.org/downloads/DDNet-19.0-win32.zip',
+    },
+    {
+      label: 'Linux x86',
+      url: 'https://ddnet.org/downloads/DDNet-19.0-linux_x86.tar.xz',
+    },
+    {
+      label: 'Linux x86-64',
+      url: 'https://ddnet.org/downloads/DDNet-19.0-linux_x86_64.tar.xz',
+    },
+    {
+      label: 'macOS (arm64 & x86-64)',
+      url: 'https://ddnet.org/downloads/DDNet-19.0-macos.dmg',
+    },
+    {
+      label: 'Android',
+      url: 'https://ddnet.org/downloads/DDNet-19.0.apk',
+    },
+    {
+      label: 'Source',
+      url: 'https://ddnet.org/downloads/DDNet-19.0.tar.xz',
+    },
+  ];
 }
